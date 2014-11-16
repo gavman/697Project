de1e2a36d0815c211e869589e843c5a07cc4096c
kbrennan234 <kbrennan@andrew.cmu.edu>
2014-11-16 10:37:06 -0500

    Started clustering framework

--- cluster_algorithm.py
+++ cluster_algorithm.py
@@ -4,8 +4,21 @@
 	#takes number of clusters as an input 1
 	#takes generation of data as input 2
 	#returns the clusters that get passed into the genetic algorithm
-def do_cluster_algorithm(num_clusters, old_gen, fitness_function):
-	return
+def do_cluster_algorithm(num_clusters, results, cluster_range):
+  # run k-means with the desired number of clusters
+  min_error = kMeans(results, num_clusters);
+  min_clusters = num_clusters;
+
+  # run
+  for i in range(1, cluster_range):
+    fitness = kMeans(results, num_clusters + i);
+    
+    tmp_num = num_clusters - 1;
+    if (tmp_num <= 0):
+      continue;
+    fitness = kMeans(results, num_clusters - i);
+    
+  return
 
 
 #function distance_measure
@@ -13,4 +26,4 @@ def do_cluster_algorithm(num_clusters, old_gen, fitness_function):
 	#takes a data point as input 2
 	#returns the distance between input 1 and input 2
 def distance_measure(data_val_one, data_val_two):
-	return
+  return
