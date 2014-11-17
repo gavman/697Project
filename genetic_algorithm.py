c72d0530fd19e1d2340e333ced8f4d6ed4a7fc05
Gavi Adler <gaviadler@gmail.com>
2014-11-17 14:16:48 -0500

    naive bayes basically done

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import string
+import copy
 
 #function do_genetic_algorithm
     #takes a scaling factor which is the exploration/exploitation trade-off
@@ -11,32 +12,66 @@ import string
 def do_genetic_algorithm(phi, old_gen):
     return
 
-#words is a list of words
+#test set of words for naive_bayes, has 80% accuracy
+"""
+words = list()
+words.append('politics')
+words.append('president')
+words.append('gop')
+words.append('democrat')
+words.append('republicans')
+words.append('war')
+words.append('bush')
+words.append('iraq')
+"""
+
+
+#words is a python list of key words
 def naive_bayes(words):
+    #topics
+    topic_min = 1
+    topic_max = 2
+
     #TODO split into training and testing
     data = pd.DataFrame.from_csv('titles_nice.csv')
-    
+    training_set = data
+    testing_set = data
+
+
     priors = list()
     mu = pd.DataFrame([])
     sigma = pd.DataFrame([])
-    
-    total_count = data.Topic.count()
-    for yk in xrange(int(data.Topic.min(1), int(data.Topic.max(1)):
+
+    total_count = training_set.Topic.count()
+    for yk in xrange(topic_min, topic_max+1):
         this_data = data[data.Topic == yk]
         class_count = this_data.Topic.count()
         prior = float(class_count)/total_count
         priors.append(prior)
-    
-    #train
-    titles = data.Titles()
-    word_probs = list()
-    for word in words:
-        word_count = data[data.Title.str.contains(word)]
-        word_count_one = float(word_count[data.Topic == 1].Title.count())
-        word_count_two = float(word_count[data.Topic == 2].Title.count())
-        word_count_all = word_count.Title.count()
-        word_probs = (word_count_one/word_count_all, word_count_two/word_count_all)
 
-    #TODO: test on testing set, return % correct
+    #train
+    word_probs = dict()
+    for word in words:
+        word_titles = training_set[training_set.Title.str.contains(word)]
+        this_word_probs = list()
+        for topic in xrange(topic_min, topic_max+1):
+            word_count = float(word_titles[training_set.Topic == topic].Title.count())
+            word_count_all = word_titles.Title.count()
+            this_word_probs.append(word_count/word_count_all)
+        word_probs[word] = this_word_probs
+
     #test
-    for title in 
+    num_correct = 0
+    for i in xrange(testing_set.Title.count()):
+        title = testing_set.Title.values[i]
+        probs = copy.copy(priors)
+        #multiply the priors by likelihood of topic given each word in our set
+        for found_word in [word for word in words if word in title]:
+            for topic in xrange(topic_min, topic_max+1):
+                probs[topic-topic_min] *= word_probs.get(found_word)[topic-topic_min]
+        #pick the topic with the greatest probability
+        guess = probs.index(max(probs)) + topic_min
+        if testing_set.Topic.values[i] == guess:
+            num_correct += 1
+
+print float(num_correct)/testing_set.Title.count()
