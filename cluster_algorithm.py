83094e74b931f615ca4baef2ae5237d45d5ab2b1
kbrennan234 <kbrennan@andrew.cmu.edu>
2014-11-24 11:12:46 -0500

    K-means

--- cluster_algorithm.py
+++ cluster_algorithm.py
@@ -45,46 +45,48 @@ def kmeans(data, num_means):
     num_rows = len(data)
     maxes = data.max(axis = 1)
     mins = data.min(axis = 1)
-
+        
     #means: each column is a n-dimensional mean
     #create random initial means
-    means = pd.DataFrame(np.zeros((num_rows, num_means)))
+    means = pd.DataFrame(np.zeros((num_rows+1, num_means)))
+    new_means = means.copy()
     for mean in xrange(num_means):
-        for row in xrange(len(maxes)):
+        new_means[mean].ix[num_rows] = 1
+        for row in xrange(num_rows):
             min = int(round(mins.ix[row]))
             max = int(round(maxes.ix[row]))
-            means[mean].ix[row] = float(random.randint(min, max))
+            new_means[mean].ix[row] = float(random.randint(min, max))
 
-    old_means = pd.DataFrame(np.zeros((num_rows, num_means)))
-    #iterate
-    while (not means.equals(old_means)):
-        old_means = means.copy()
+    # Iterate until means do not change between iterations
+    while (not means.equals(new_means)):
+        # Copy means from last iteration and reset new means
+        means = new_means.copy()
+        new_means = pd.DataFrame(np.zeros((num_rows+1, num_means)))
+        error = 0
+        
         clusters = pd.Series(-1, np.arange(num_rows))
-        #find 
+        # Find closest cluster for each data point
         for i in xrange(len(data.columns)):
             cluster_name = data.columns[i]
             cluster = data[cluster_name]
             shortest_dis = -1
+            min_mean = 0
             for mean in means.columns:
-                dis = np.linalg.norm(cluster.sub(means[mean], axis=0))
-                if (shortest_dis == -1 or dis < shortest_dis):
-                    shortest_dis = dis
-                    clusters[cluster_name] = mean
-        
+                # Check for empty cluster
+                if (means[mean].ix[num_rows] != 0):
+                    dis = np.linalg.norm(cluster.sub(means[mean].ix[0:num_rows-1], axis=0))
+                    if (shortest_dis == -1 or dis < shortest_dis):
+                        shortest_dis = dis
+                        min_mean = mean
+
+            error += shortest_dis
+            for row in xrange(len(maxes)):
+                new_means[min_mean].ix[0:num_rows-1] += cluster
+            new_means[mean].ix[num_rows] += 1
+
+        # Compute new means for next iteration
         for mean in means.columns:
-            num_columns = 0
-            this_cluster = pd.DataFrame()
-            for i in xrange(len(clusters)):
-                cluster = clusters[i]
-                if cluster == mean:
-                    this_cluster[len(this_cluster.columns)] = data[i]
-                    num_columns += 1
-            means[mean] = this_cluster.mean(axis=1)
-    
-    #find total error
-    error = 0.0
-    for data_point in data.columns:
-        mean = means[clusters[data_point]]
-        error += np.linalg.norm(data[data_point].sub(mean, axis=0))
+            if (new_means[mean].ix[num_rows] != 0):
+                new_means[mean] = new_means[mean] / new_means[means].ix[num_rows]
 
     return error/len(data.columns)
